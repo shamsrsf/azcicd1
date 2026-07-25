5c1c672286318c8a2c1c9350c1a5d7a80952d730
yousufrays-gh <yousufrays@outlook.com>
2026-07-25 15:27:24 -0500

    update17

--- 05_Web_Sql_database/outputs.tf
+++ 05_Web_Sql_database/outputs.tf
@@ -1,7 +1,7 @@
-output "webapp_url" {
-  value = azurerm_linux_web_app.app.default_hostname 
-}
+#output "webapp_url" {
+#  value = azurerm_linux_web_app.app.default_hostname 
+#}
 
-output "webapp_ips" {
-  value = azurerm_linux_web_app.app.outbound_ip_addresses
-}
+#output "webapp_ips" {
+#  value = azurerm_linux_web_app.app.outbound_ip_addresses
+#}
