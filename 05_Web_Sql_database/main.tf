e30e4cdc89e938f1890007016281c9e7b19ec064
yousufrays-gh <yousufrays@outlook.com>
2026-07-25 13:54:05 -0500

    udpate9

--- 05_Web_Sql_database/main.tf
+++ 05_Web_Sql_database/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
 
   tags = {
-    environment = "preproduction"
+    environment = "dev"
   }
 }
 
@@ -57,7 +57,7 @@ resource "azurerm_mssql_database" "db" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "tfstorage013rays"
+  name                     = "tfstorage014rays"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
